de7a8f0e2762fb8923945d659ca52fc132d90a33
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-07 23:45:17 +0700

    Modified:
    - Create text file for train|valid|test dataset

--- Preprocessing/MakeTrainValText.py
+++ Preprocessing/MakeTrainValText.py
@@ -9,18 +9,86 @@ Caffe style
 '''
 
 import yaml
-import os
-import numpy
+from os import listdir
+from os.path import abspath, join, isdir, isfile
+from random import shuffle
+
+
+def getListByIndices(list, indices):
+    newList = [list[idx] for idx in indices]
+    return newList
+
 
 with open('../Utils/PathConfig.yaml', 'r') as f:
     paths = yaml.load(f)
+TRAIN_IMG_DIR   = paths['TRAIN_IMG_DIR']
+VALID_IMG_DIR   = paths['VALID_IMG_DIR']
+TEST_IMG_DIR    = paths['TEST_IMG_DIR']
 
-trainImgDir   = paths['TRAIN_IMG_DIR']
-validImgDir   = paths['VALID_IMG_DIR']
-trainFilename = paths['TRAIN_FILENAME']
-validFilename = paths['VALID_FILENAME']
+TRAIN_DATA_FILENAME         = paths['TRAIN_DATA_FILENAME']
+VALID_DATA_FILENAME         = paths['VALID_DATA_FILENAME']
+TEST_DATA_FILENAME          = paths['TEST_DATA_FILENAME']
 
-sortedTrainDirs = sorted([name for name in os.listdir(trainImgDir)
-                          if os.path.isdir(os.path.join(trainImgDir, name))])
+
+################################
+#     Training dataset         #
+################################
+sortedTrainDirs = sorted([join(abspath(TRAIN_IMG_DIR), name) for name in listdir(TRAIN_IMG_DIR)
+                          if isdir(join(TRAIN_IMG_DIR, name))])
+allTrainFiles = []
+allTrainFilesLabelNumber = []
+allTrainFilesLabelText = []
+for idx, trainDir in enumerate(sortedTrainDirs):
+    label = trainDir.split('/')[-1]
+    sortedTrainFiles = sorted([join(abspath(trainDir), name) for name in listdir(trainDir)
+                              if isfile(join(trainDir, name))])
+    allTrainFiles.extend(sortedTrainFiles)
+    for i in range(len(sortedTrainFiles)):
+        allTrainFilesLabelNumber.append(idx)
+        allTrainFilesLabelText.append(label)
+
+# Random data before save
+newIdx = list(range(len(allTrainFiles)))
+shuffle(newIdx)
+allTrainFiles = getListByIndices(allTrainFiles, newIdx)
+allTrainFilesLabelNumber = getListByIndices(allTrainFilesLabelNumber, newIdx)
+allTrainFilesLabelText = getListByIndices(allTrainFilesLabelText, newIdx)
+
+# Save metadata to text
+file = open(TRAIN_DATA_FILENAME, 'wb')
+for idx in range(len(allTrainFiles)):
+    file.write('%s %d %s \n' % (allTrainFiles[idx], allTrainFilesLabelNumber[idx], allTrainFilesLabelText[idx]))
+file.close()
+
+################################
+#     Validation dataset       #
+################################
+sortedValidFiles = sorted([join(abspath(VALID_IMG_DIR), name) for name in listdir(VALID_IMG_DIR)
+                          if isfile(join(VALID_IMG_DIR, name))])
+allValidFiles = sortedValidFiles
+file = open('ILSVRC2010_validation_ground_truth.txt')
+allValidFilesLabelNumber = tuple(file)
+allValidFilesLabelNumber = [int(validLabel.strip()) for validLabel in allValidFilesLabelNumber]
+file.close()
+
+# Save metadata to text
+file = open(VALID_DATA_FILENAME, 'wb')
+for idx in range(len(allValidFiles)):
+    file.write('%s %d \n' % (allValidFiles[idx], allValidFilesLabelNumber[idx]))
+file.close()
+
+################################
+#     Test dataset             #
+################################
+sortedTestFiles = sorted([ join(abspath(TEST_IMG_DIR), name) for name in listdir(TEST_IMG_DIR)
+                        if isfile(join(TEST_IMG_DIR, name))])
+allTestFiles = sortedTestFiles
+
+# Save metadata to text
+file = open(TEST_DATA_FILENAME, 'wb')
+for idx in range(len(allTestFiles)):
+    file.write('%s \n' % (allTestFiles[idx]))
+file.close()
+
 
 
