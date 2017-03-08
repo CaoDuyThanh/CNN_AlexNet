6350edc5244c99909166cd23b9328af80f08b953
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-08 10:32:16 +0700

    Modified:
    - Calculate mean image from train dataset

--- Preprocessing/CalculateMean.py
+++ Preprocessing/CalculateMean.py
@@ -9,35 +9,49 @@ Caffe style
 '''
 
 import yaml
-import numpy
+from pylab import *
 from PIL import Image
+from Utils.DataHelper import *
 
 with open('../Utils/PathConfig.yaml', 'r') as f:
     paths = yaml.load(f)
 
 TRAIN_DATA_FILENAME         = paths['TRAIN_DATA_FILENAME']
+VALID_DATA_FILENAME         = paths['VALID_DATA_FILENAME']
+TEST_DATA_FILENAME          = paths['TEST_DATA_FILENAME']
+
+################################
+#     Load metadata            #
+################################
+dataHelper = DatasetHelper(
+    trainFilePath = TRAIN_DATA_FILENAME,
+    validFilePath = VALID_DATA_FILENAME,
+    testFilePath = TEST_DATA_FILENAME
+)
+allTrainData = dataHelper.AllTrainData
+allValidData = dataHelper.AllValidData
+allTestData  = dataHelper.AllTestData
 
 ################################
 #     Training dataset         #
 ################################
-file = open(TRAIN_DATA_FILENAME)
-allSamples = tuple(file)
-file.close()
-
-numSamples = 0
-allTrainData = []
-for sample in allSamples:
-    if (sample != ''):
-        numSamples += 1
-        allTrainData.append(sample.split(' '))
-
-meanImage = numpy.zeros((3, 224, 224), dtype = 'float64')
+meanImage = numpy.zeros((256, 256, 3), dtype = 'float64')
+counter = 0
 for trainData in allTrainData:
+    counter += 1
+    if counter % 5000 == 0:
+        print ('Process %d / %d images !' % (counter, allTrainData.__len__()))
+
     imagePath = trainData[0]
     image = Image.open(imagePath)
-    image = Image.fromarray(numpy.rollaxis(image, 0, 3))
     meanImage += image
-meanImage /= numSamples
+meanImage /= allTrainData.__len__()
+
+file = open('mean_image.pkl', 'wb')
+pickle.dump(meanImage, file, -1)
+file.close()
+
+
 
 
 
