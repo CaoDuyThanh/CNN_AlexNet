ff265e5597856ed553e6bfc9a8e0f7e43f2161d2
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-09 23:28:49 +0700

    Delete:
    - Softmax layer was added in Hidden layer

--- Layers/SoftmaxLayer.py
+++
@@ -1,9 +0,0 @@
-from theano.tensor.nnet.nnet import softmax
-
-class SoftmaxLayer:
-    def __init__(self,
-                 input):
-        self.Input = input
-
-    def Output(self):
-        return softmax(self.Input)
