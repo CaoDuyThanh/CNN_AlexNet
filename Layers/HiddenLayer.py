5d55775ef6eef6f2392b5c3ff0226a6be5f1c070
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-09 23:27:47 +0700

    Modified:
    - Refactor code

--- Layers/HiddenLayer.py
+++ Layers/HiddenLayer.py
@@ -10,55 +10,46 @@ class HiddenLayer:
                  numOut,                # Number reurons out of layer
                  activation = T.tanh,   # Activation function
                  W = None,
-                 b = None
-                 ):
+                 b = None):
         # Set parameters
+        self.Rng = rng
         self.Input = input
         self.NumIn = numIn
         self.NumOut = numOut
         self.Activation = activation
+        self.W = W
+        self.b = b
 
+        self.createModel()
+
+    def createModel(self):
         # Create shared parameters for hidden layer
-        if W is None:
+        if self.W is None:
             """ We create random weights (uniform distribution) """
             # Create boundary for uniform generation
             wBound = numpy.sqrt(6.0 / (self.NumIn + self.NumOut))
             self.W = theano.shared(
                 numpy.asarray(
-                    rng.uniform(
-                        low=-wBound,
-                        high=wBound,
-                        size=(self.NumIn, self.NumOut)
+                    self.Rng.uniform(
+                        low  = -wBound,
+                        high =  wBound,
+                        size = (self.NumIn, self.NumOut)
                     ),
-                    dtype=theano.config.floatX
+                    dtype = theano.config.floatX
                 ),
-                borrow=True
+                borrow = True
             )
-        else:
-            """ Or simply set weights from parameter """
-            self.W = W
 
-        if b is None:
+        if self.b is None:
             """ We create zeros bias """
             # Create bias
             self.b = theano.shared(
                 numpy.zeros(
                     shape = (self.NumOut, ),
-                    dtype=theano.config.floatX
+                    dtype = theano.config.floatX
                 ),
-                borrow=True
+                borrow = True
             )
-        else:
-            """ Or simply set bias from parameter """
-            self.b = b
 
-    def Output(self):
-        output = T.dot(self.Input, self.W) + self.b
-        if self.Activation is None:
-            return output
-        else:
-            return self.Activation(output)
-
-    def Params(self):
-        return [self.W, self.b]
-
+        self.Params = [self.W, self.b]
+        self.Output = self.Activation(T.dot(self.Input, self.W) + self.b)
