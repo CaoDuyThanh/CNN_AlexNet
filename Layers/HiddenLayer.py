c44db3e465636ef536fcb1a00db967785dfe6b97
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-25 11:11:11 +0700

    Modified:
    - Add dropout to hidden layer

--- Layers/HiddenLayer.py
+++ Layers/HiddenLayer.py
@@ -7,22 +7,31 @@ import theano.tensor as T
 class HiddenLayer:
     def __init__(self,
                  rng,                   # Random seed
+                 theanoRng,             # Random theano seed
                  input,                 # Data input
                  numIn,                 # Number neurons of input
                  numOut,                # Number reurons out of layer
                  activation = T.tanh,   # Activation function
+                 dropout = None,         # Dropout parameter
                  W = None,
                  b = None):
         # Set parameters
         self.Rng = rng
+        self.TheanoRng = theanoRng
         self.Input = input
         self.NumIn = numIn
         self.NumOut = numOut
         self.Activation = activation
+        self.Dropout = dropout
         self.W = W
         self.b = b
 
         self.createModel()
+
+    def dropout(self, input, corruptionLevel):
+        return self.TheanoRng.binomial(size=input.shape, n=1,
+                                       p = 1 - corruptionLevel,
+                                       dtype=theano.config.floatX) * input
 
     def createModel(self):
         # Create shared parameters for hidden layer
@@ -55,6 +64,10 @@ class HiddenLayer:
 
         self.Params = [self.W, self.b]
         self.Output = self.Activation(T.dot(self.Input, self.W) + self.b)
+
+        if self.Dropout is not None:
+            self.Output = self.dropout(self.Output, self.Dropout)
+
 
     def LoadModel(self, file):
         [param.set_value(cPickle.load(file), borrow=True) for param in self.Params]
